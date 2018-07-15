c328719801bf4f966f98754e60c17823f05086cb
danielVg <danielveragi@gmail.com>
2018-07-15 10:41:22 +0200

    changing zsh install
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- apps/zsh.sh
+++ apps/zsh.sh
@@ -2,13 +2,12 @@
 
 # Install through the package manager
 echo "Installing zsh"
-# sudo apt-get install zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+
+# Set Syntax highlighting
+echo "Installing Sytanx highlighting"
+cd ~ && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git  
 
 # Set default shell
 echo "Setting zsh to default shell"
-# chsh -s $(which zsh)
 chsh -s `which zsh`
-
-# echo "The new Shell is: "
-# echo $SHELL
