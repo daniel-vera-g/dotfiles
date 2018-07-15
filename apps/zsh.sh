e6c5ce8f2679c07b05711a51a3d620ddeaa82021
danielVg <danielveragi@gmail.com>
2018-07-15 10:13:04 +0200

    changed install zsh
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- apps/zsh.sh
+++ apps/zsh.sh
@@ -2,11 +2,13 @@
 
 # Install through the package manager
 echo "Installing zsh"
-sudo apt-get install zsh
+# sudo apt-get install zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
 # Set default shell
 echo "Setting zsh to default shell"
-chsh -s $(which zsh)
+# chsh -s $(which zsh)
+chsh -s `which zsh`
 
 echo "The new Shell is: "
 echo $SHELL
