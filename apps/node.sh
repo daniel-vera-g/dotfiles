1efbc83ebb74145bbb69abdb9592cbb66dce8a9c
danielVg <danielveragi@gmail.com>
2018-07-15 11:32:59 +0200

    added curl
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- apps/node.sh
+++ apps/node.sh
@@ -1,5 +1,10 @@
 # Install nodejs & npm 
 
 echo "Installing nodejs & npm"
+
+echo "First make sure we have curl installe"
+sudo apt-get install curl
+
+echo "Now let's get node & npm up and running"
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
