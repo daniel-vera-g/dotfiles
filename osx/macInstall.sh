187a296619961f5ad237a5fd9d9fde7aae9bed12
Daniel <DanielVG@Daniels-MacBook-Air.local>
2018-08-05 20:00:32 +0200

    osx setup

--- osx/macInstall.sh
+++ osx/macInstall.sh
@@ -82,34 +82,33 @@ echo "###################################### DOTFILES ##########################
 
 # Bunch of symlinks
 
-echo "Creating simlinks for git"
-ln -sfv "$dir/shell/git/.gitconfig" ~
-ln -sfv "$dir/shell/git/.gitignore_global" ~
-
-echo "Creating simlinks for vim"
-ln -sfv "$dir/vim/.vimrc" ~
-
-echo "Creating symlinks for Shell configuration"
-ln -sfv "$dir/shell/.bashrc" ~
-ln -sfv "$dir/shell/.hyper" ~
-ln -sfv "$dir/shell/.zshrc" ~
-ln -sfv "$dir/shell/z.sh" ~
-
 echo "Creating simlinks for the linters"
 ln -sfv "$dir/linter/.eslintrc" ~
 ln -sfv "$dir/linter/.markdownlintrc" ~
 ln -sfv "$dir/linter/.prettierrc" ~
 ln -sfv "$dir/linter/.textlintrc" ~
 
+echo "Creating simlinks for git"
+ln -sfv "$dir/shell/git/.gitconfig" ~
+ln -sfv "$dir/shell/git/.gitignore_global" ~
+
+echo "Creating simlinks for vim"
+ln -sfv "$dir/shell/vim/.vimrc" ~
+
+echo "Creating symlinks for Shell configuration"
+ln -sfv "$dir/shell/.alias" ~
+ln -sfv "$dir/shell/.exports" ~
+ln -sfv "$dir/shell/.function" ~
+ln -sfv "$dir/shell/.path" ~
+ln -sfv "$dir/shell/.tmux.conf" ~
+ln -sfv "$dir/shell/.bashrc" ~
+ln -sfv "$dir/shell/.hyper" ~
+ln -sfv "$dir/shell/.zshrc" ~
+ln -sfv "$dir/shell/z.sh" ~
+
 echo "Creating system simlinks"
-ln -sfv "$dir/system/.alias" ~
-ln -sfv "$dir/system/.exports" ~
-ln -sfv "$dir/system/.function" ~
-ln -sfv "$dir/system/.path" ~
-ln -sfv "$dir/system/.tmux.conf" ~
 ln -sfv "$dir/system/.redshfit.conf" ~/.config
 ln -sfv "$dir/system/terminator/config" ~/.config/
-ln -sfv "$dir/system/download_and_install.sh" ~
 
 echo "+++++++++++++++++++++++++++++ Done +++++++++++++++++++++++++++++"
 echo "Don't forget to configure SSH properly with key and config"
