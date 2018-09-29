2faf79fdf4c3a3bcc62342fb041706a846356b2c
Daniel VG <danielveragi@gmail.com>
2018-09-29 18:16:30 +0200

    Fixed spaceship theme issue
    Fix spaceship theme issue by removing the local variables.

--- linux/apps/spaceshipTheme.sh
+++ linux/apps/spaceshipTheme.sh
@@ -1,7 +1,13 @@
-# Install the spaceship theme
-
-sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-
-sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-
+ echo "########################################"
+ 
+ echo "Install the spaceship theme"
+ 
+ echo "########################################"
+ 
+ ZSH_CUSTOM=/home/dvg/.oh-my-zsh/custom
+ THEME_FOLDER=$ZSH_CUSTOM/themes/spaceship-prompt
+ 
+ git clone https://github.com/denysdovhan/spaceship-prompt.git $THEME_FOLDER
+ 
+ ln -s "$THEME_FOLDER/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+                                                                                            
