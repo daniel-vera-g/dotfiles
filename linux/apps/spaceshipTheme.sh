8c4c49e24d43d0bd2f3630b877bcda23864d57fc
danielVg <danielveragi@gmail.com>
2018-09-29 14:48:41 +0200

    Fixed spaceship theme issue

--- linux/apps/spaceshipTheme.sh
+++ linux/apps/spaceshipTheme.sh
@@ -1,9 +1,7 @@
 # Install the spaceship theme
 
-npm install -g spaceship-prompt
+sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
 
