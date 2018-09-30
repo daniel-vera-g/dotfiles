7ba0b398dba1ec37db69fab2ab746b32293eaa3d
Daniel VG <danielveragi@gmail.com>
2018-09-30 08:51:38 +0200

    Fix sudo for wget

--- linux/apps/Azsh.sh
+++ linux/apps/Azsh.sh
@@ -6,7 +6,7 @@ echo "************************ Installing zsh ************************"
 echo "Installing zsh"
 sudo apt-get install zsh
 sudo apt-get install git-core
-sudo https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
 # Set Syntax highlighting
 echo "Installing Sytanx highlighting"
