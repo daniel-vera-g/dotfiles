e52669438481e81a151a3020eae65ba7bcf31167
danielVg <danielveragi@gmail.com>
2019-04-12 09:29:57 +0200

    some priorisation

--- linux/apps/essentials/1-node.sh
+++ linux/apps/essentials/1-node.sh
@@ -2,9 +2,6 @@ echo "##########################################################################
 echo "************************ Installing nodejs & npm ************************"
 echo "####################################################################################"
 
-echo "First make sure we have curl installed"
-sudo apt-get install curl
-
 echo "Now let's get node & npm up and running"
 sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
