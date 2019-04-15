ca91a0313416f19514c3f07d49a3214ae3c09817
dvg <danielveragi@gmail.com>
2019-04-15 09:16:08 +0000

    ripgrp install

--- linux/apps/ripgrep.sh
+++ linux/apps/ripgrep.sh
@@ -2,4 +2,4 @@ echo "##########################################################################
 echo "************************ Installing ripgrep ************************"
 echo "####################################################################################"
 
-sudo apt-get install ripgrep
+(cd ~/downloads/ && curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb && sudo dpkg -i ripgrep_0.10.0_amd64.deb)
