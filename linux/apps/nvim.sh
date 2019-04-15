c12977f3c00dd938d0d025b86f0164182383b371
danielVg <danielveragi@gmail.com>
2019-04-15 10:49:18 +0200

    Python packages for nvim extensions

--- linux/apps/nvim.sh
+++ linux/apps/nvim.sh
@@ -7,3 +7,7 @@ sudo apt-get install software-properties-common && sudo add-apt-repository ppa:n
 # Python requirements
 
 sudo apt-get install python-dev python-pip python3-dev python3-pip
+
+# Install python packages for python Plugins like YCM
+
+sudo pip2 install --upgrade pynvim &&  sudo pip3 install --upgrade pynvim
