4aa801aa80f17f33b7bd62ace97fc65ff81d13b4
dvg <danielveragi@gmail.com>
2019-04-15 07:24:43 +0000

    Install script for nvim

--- linux/apps/nvim.sh
+++ linux/apps/nvim.sh
@@ -7,12 +7,3 @@ sudo apt-get install software-properties-common && sudo add-apt-repository ppa:n
 # Python requirements
 
 sudo apt-get install python-dev python-pip python3-dev python3-pip
-
-# Remove alternatives
-
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --config vi
-sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-sudo update-alternatives --config vim
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-sudo update-alternatives --config editor
