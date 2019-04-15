5f43af7ec88c875b09b0499bff8dcc6c5eb0d8ab
danielVg <danielveragi@gmail.com>
2019-04-15 15:55:39 +0200

    system ordering file

--- linux/install.sh
+++ linux/install.sh
@@ -107,6 +107,7 @@ ln -sfv "$dir/shell/glances/glances.conf" ~/.config/
 echo "Creating system simlinks"
 ln -sfv "$dir/system/.redshfit.conf" ~/.config
 ln -sfv "$dir/system/terminator/config" ~/.config/
+ln -sfv "$dir/system/ordering-override.keyfile" ~/.local/share/indicators/application/ordering-override.keyfile
 
 echo "+++++++++++++++++++++++++++++ Done +++++++++++++++++++++++++++++"
 echo "Don't forget to configure SSH properly with key and config"
