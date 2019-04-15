30d920c79e1f7b339bd0918ee03ad19e07e94503
dvg <danielveragi@gmail.com>
2019-04-15 09:21:57 +0000

    fd install script & fix for downloads location

--- linux/apps/ripgrep.sh
+++ linux/apps/ripgrep.sh
@@ -2,4 +2,4 @@ echo "##########################################################################
 echo "************************ Installing ripgrep ************************"
 echo "####################################################################################"
 
-(cd ~/downloads/ && curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb && sudo dpkg -i ripgrep_0.10.0_amd64.deb)
+(cd ~/Downloads/ && curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb && sudo dpkg -i ripgrep_0.10.0_amd64.deb)
