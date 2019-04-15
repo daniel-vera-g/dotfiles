361e88c0780b6694287fe19e95dacf129e6b3f7a
danielVg <danielveragi@gmail.com>
2019-04-15 10:58:18 +0200

    simlink for nvim config

--- linux/apps/nvim.sh
+++ linux/apps/nvim.sh
@@ -11,3 +11,8 @@ sudo apt-get install python-dev python-pip python3-dev python3-pip
 # Install python packages for python Plugins like YCM
 
 sudo pip2 install --upgrade pynvim &&  sudo pip3 install --upgrade pynvim
+
+# Install PLug
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
