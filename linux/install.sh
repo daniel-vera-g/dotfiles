dc733584c435f0dfaa1462c6f9b5628150fcb0be
danielVg <danielveragi@gmail.com>
2019-04-16 09:36:40 +0200

    gpg config

--- linux/install.sh
+++ linux/install.sh
@@ -108,6 +108,7 @@ echo "Creating system simlinks"
 ln -sfv "$dir/system/.redshfit.conf" ~/.config
 ln -sfv "$dir/system/terminator/config" ~/.config/
 ln -sfv "$dir/system/ordering-override.keyfile" ~/.local/share/indicators/application/ordering-override.keyfile
+ln -sfv "$dir/system/gpg.conf" ~/.gpg/
 
 echo "+++++++++++++++++++++++++++++ Done +++++++++++++++++++++++++++++"
 echo "Don't forget to configure SSH properly with key and config"
