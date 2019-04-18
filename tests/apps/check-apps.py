d6607186276de820975627009d28e0dbbdd077eb
danielVg <danielveragi@gmail.com>
2019-04-18 12:20:02 +0200

    Some function refactoring

--- tests/apps/check-apps.py
+++ tests/apps/check-apps.py
@@ -6,10 +6,6 @@ import os
 import re
 import subprocess
 
-# List of applications that have to be tested manually
-blacklist = ["exfat", "cleanup"]
-appsToCheck = {}
-
 '''Return a list of file with path '''
 def getListOfFiles(dirName):
     # create a list of file and sub directories
@@ -50,32 +46,27 @@ def extractFileNamesFromPath(pathOfFiles):
 
 ''' Check different install methods for each app '''
 def checkInstalled(listApps):
-    # TODO add better handling of dict
-    # (not good to add as argument and have as global variable?)
-
-    # Remove apps from blacklist
-    listApps = [e for e in listApps if e not in blacklist]
-    # Dictionary to store install status of applications
-    appsToCheck = {el: False for el in listApps}
-
     # Iterate through apps in list and check if installed
-    for app in appsToCheck:
+    for app in listApps:
         # Get into right format(String)
         app = str(app)
         # Check for different methods
-        if not appsToCheck[app]:
-            checkBinaries(app)
-        if appsToCheck[app] == False:
-            print(appsToCheck[app])
-            checkNpm(app)
-        if not appsToCheck[app]:
-
+        if not listApps[app]:
+            appStatus = checkBinaries(app)
+            listApps = setStatusProgramm(listApps, app, appStatus)
+        if not listApps[app]:
+            appStatus = checkNpm(app)
+            listApps = setStatusProgramm(listApps, app, appStatus)
+        if not listApps[app]:
+	
             # TODO Check for fonts(powerline & firacode)?
             # TODO Check marker from home directory
             # TODO httpie, git-flow, system-load-indicator, solar, miktex, safe-eyes & spaceship-theme -> Check other installment folders"
 
-            checkAll(app)
-        if not appsToCheck[app]:
+            appStatus = checkAll(app)
+            listApps = setStatusProgramm(listApps, app, appStatus)
+
+        if not listApps[app]:
            print(app + "could not be found and is probably not installed.\n")
            print("Please check manually ") 
 
@@ -83,48 +74,63 @@ def checkInstalled(listApps):
             # Show specific part -> search with grep 
             # TODO add all failed applications to list, check better and show afterwards
 
-''' Check binaries '''
+''' Check binaries and returns new Status '''
 def checkBinaries(app):
     try:
         res = subprocess.call(["which", app])
-        print(res)
         if res == 0:
-            appsToCheck[app] = True
-            print(appsToCheck[app])
+            return True
         else:
             print(
             app + " could not be found as installed with the binary search")
             print("Proceding with other methods...")
+            return False
     except subprocess.CalledProcessError as e:
         print("There was an error searching for the binaries of :" + app)
         print(e)
 
-''' Checking for global npm package installations '''               
+''' Checking for global npm package installations and returns new Status '''               
 def checkNpm(app):
     try:
         # TODO remove shell=True for security reasons
         res = subprocess.call(["npm", "list", "-g", app, "|", "grep", app], shell=True)
         if res == 0:
-            appsToCheck[app] = True
+            return True
         else:
             print(
             app + " could not be found as installed in the local npm directory.")
             print("Proceding with other methods...")
+            return False
     except subprocess.CalledProcessError as e:
         print("Error checking existence of npm package: " + app)
         print(e)
 
-''' Checking system wide(worst case) '''
+''' Checking system wide(worst case) and returns new Status '''
 def checkAll(app):
     try:
         res = subprocess.call(["sudo", "find", "/", "-name", "'*"+app+"'", "|", "grep", app])
         if res == 0:
-            appsToCheck[app] = True
+            return True
         else:
-            print(app + " could not be found in the system wide search.")
+            return False
     except subprocess.CalledProcessError as e:
         print("Error at the system wide search after: " + app)
         print(e)
+
+''' Return Dictionary with programms to check '''
+def prepareDictionary(listApps):
+    # List of applications that have to be tested manually
+    blacklist = ["exfat", "cleanup"]
+    # Remove apps from blacklist
+    listApps = [e for e in listApps if e not in blacklist]
+    # Dictionary to store install status of applications
+    appsToCheck = {el: False for el in listApps}
+    return appsToCheck
+    
+''' Change the value of an application and return updated dictionary '''
+def setStatusProgramm(listOfAppsToChange, app, value):
+   listOfAppsToChange[app] = value
+   return listOfAppsToChange 
 
 def main():
 
@@ -136,8 +142,10 @@ def main():
     # Remove file path & get only the file names
     fileNames = extractFileNamesFromPath(listOfFiles)
 
+    programms = prepareDictionary(fileNames)
+
     # Check apps for install status
-    checkInstalled(fileNames)
+    checkInstalled(programms)
 
 
 if __name__ == '__main__':
