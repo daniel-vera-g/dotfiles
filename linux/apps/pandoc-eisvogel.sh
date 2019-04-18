7b05000080d8d5b0e27b967cc9eb4b494141b69a
danielVg <danielveragi@gmail.com>
2019-04-18 18:57:54 +0200

    changed pandoc eisvogel template

--- linux/apps/pandoc-eisvogel.sh
+++ linux/apps/pandoc-eisvogel.sh
@@ -2,4 +2,4 @@ echo "##########################################################################
 echo "************************ Installing Pandoc eisvogel template ************************"
 echo "####################################################################################"
 
-(mkdir -p ~/workspace/tools/ && cd ~/workspace/tools/ && git clone https://github.com/Wandmalfarbe/pandoc-latex-template.git && ln -sf pandoc-latex-template/eisvogel.tex  ~/.pandoc/templates/eisvogel.latex)
+(mkdir -p ~/workspace/tools/ && cd ~/workspace/tools/ && git clone https://github.com/Wandmalfarbe/pandoc-latex-template.git && mkdir ~/.pandoc/templates/ && ln -sf pandoc-latex-template/eisvogel.tex  ~/.pandoc/templates/eisvogel.latex)
