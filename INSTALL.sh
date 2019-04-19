667d10b8616acc04ce6ea6694513918b9bfd9a9f
danielVg <danielveragi@gmail.com>
2019-04-19 15:36:40 +0200

    sh ending to executable

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,4 +3,4 @@
 # MASTER INSTALLATION FILE
 
 export DEBIAN_FRONTEND=noninteractive
-cd ~/dotfiles/linux/ && chmod +x ./install.sh && ./install 2>&1 | tee install-logs.txt
+cd ~/dotfiles/linux/ && chmod +x ./install.sh && ./install.sh 2>&1 | tee install-logs.txt
