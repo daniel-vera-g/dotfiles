455e4ca040598812e9a18877ef501e5a9d487639
danielVg <danielveragi@gmail.com>
2019-04-19 10:24:21 +0200

    better folder structure & declarative name

---
+++ tests/apps/helper/app-test.sh
@@ -0,0 +1,50 @@
+echo "####################################################################################"
+echo "************************ Testing the application installation ************************"
+echo "####################################################################################"
+
+######################################
+
+# Installing Applications
+
+echo "###################################### STARTING WITH ALL THE INSTALLING ###################################### "
+
+######################################
+
+# Create download folder
+mkdir ~/Downloads/
+
+dir=~/dotfiles/linux                    # dotfiles directory
+echo "The new dotfiles repository is: $dir"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# First some cleanup
+echo "###################################### DOING SOME CLEANUP FIRST ###################################### "
+chmod +x $dir/apps/cleanup/cleanup.sh && $dir/apps/cleanup/cleanup.sh
+
+# Install priority applications
+echo "###################################### INSTALLING ESSENTIAL APPLICATIONS ###################################### "
+for e in $dir/apps/essentials/*.sh
+do
+  echo "Installing essential app $e"
+  chmod +x $e && $e
+done 
+
+# First install basic apps
+for f in $dir/apps/* $dir/apps/npm-packages/*;
+do
+  echo "Processing $f file..."
+  chmod +x $f && $f
+done
+
+# Do some Testing
+echo "Now doing some testing:"
+cd ~/dotfiles/tests/apps/helper/
+chmod +x ./check-apps.py && python ./check-app-status.py
+
+echo "####################################################################################"
+echo "************************ Installing apps ready ************************"
+echo "####################################################################################"
