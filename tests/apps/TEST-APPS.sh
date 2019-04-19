3a16f1c2030c9199dbbf39975215dd7c421fe04a
danielVg <danielveragi@gmail.com>
2019-04-19 10:42:53 +0200

    gitignore for log files

--- tests/apps/TEST-APPS.sh
+++ tests/apps/TEST-APPS.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
-cd ~/dotfiles/tests/apps && chmod +x ./helper/app-test.sh && ./helper/app-test.sh 2>&1 | tee install-logs.txt
+cd ~/dotfiles/tests/apps && chmod +x ./helper/app-test.sh && ./helper/app-test.sh 2>&1 | tee apps-install-logs.txt
--- tests/shell/CONFIG-TESTS.sh
+++ tests/shell/CONFIG-TESTS.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
-cd ~/dotfiles/tests/config && chmod +x ./helper/config-tests.sh && ./helper/config-tests.sh 2>&1 | tee install-logs.txt
+cd ~/dotfiles/tests/config && chmod +x ./helper/config-tests.sh && ./helper/config-tests.sh 2>&1 | tee config-install-logs.txt
