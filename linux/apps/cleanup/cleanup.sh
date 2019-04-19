8cd0e0f5def2b41ddf0b7c8758ec6bdcf9c631b2
danielVg <danielveragi@gmail.com>
2019-04-19 09:41:27 +0200

    more cleanup

--- linux/apps/cleanup/cleanup.sh
+++ linux/apps/cleanup/cleanup.sh
@@ -2,4 +2,4 @@ echo "##########################################################################
 echo "************************ Do some clean up before installing ************************"
 echo "####################################################################################"
 
-sudo apt update -y && sudo apt upgrade -y && sudo apt-get -f install
+sudo apt update -y && sudo apt upgrade -y && sudo apt-get -f install && apt --fix-broken install
