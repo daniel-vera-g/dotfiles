3e713d149c023e1cdb04b7804f3d611cd4b109f4
danielVg <danielveragi@gmail.com>
2019-04-19 15:17:10 +0200

    Changed shebang the right way

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+ #!/bin/sh
 
 # MASTER INSTALLATION FILE
 
--- tests/shell/CONFIG-TESTS.sh
+++ tests/shell/CONFIG-TESTS.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+ #!/bin/sh
 
 export DEBIAN_FRONTEND=noninteractive
 cd ~/dotfiles/tests/config && chmod +x ./helper/config-tests.sh && ./helper/config-tests.sh 2>&1 | tee config-install-logs.txt
