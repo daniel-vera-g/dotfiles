6a59cfc242d7d2c9840657ef43e678e33b10e810
danielVg <danielveragi@gmail.com>
2019-12-13 10:35:14 +0100

    npm apps

---
+++ linux/apps/npm/eslint.sh
@@ -0,0 +1,10 @@
+echo "####################################################################################"
+echo "************************ Installing different node packages ************************"
+echo "####################################################################################"
+
+# Install different node packages
+
+# Eslint 
+
+echo "installing eslint"
+sudo npm install -g eslint
