19a7b608c6ee90fdbb2aee718cf27b52354bdd8c
danielVg <danielveragi@gmail.com>
2019-12-13 10:29:10 +0100

    dpkg apps

---
+++ linux/apps/dpkg/nnn.sh
@@ -0,0 +1,9 @@
+echo "####################################################################################"
+echo "************************ Installing nnn ************************"
+echo "####################################################################################"
+
+# Install dependencies
+sudo apt-get install -y pkg-config libncursesw5-dev libreadline6-dev && sudo apt-get -f install
+
+# Install nnn
+(cd ~/Downloads/ && curl -LO https://github.com/jarun/nnn/releases/download/v2.4/nnn_2.4-1_ubuntu18.04.amd64.deb && sudo dpkg -i nnn_2.4-1_ubuntu18.04.amd64.deb)
