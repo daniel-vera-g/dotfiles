742aa374e4871fc44884a5dc0802f6acbcc3e51d
danielVg <danielveragi@gmail.com>
2020-04-01 22:52:43 +0200

    location change & org

--- linux/shell/scripts/shortcuts/tmux/guake.sh
+++ linux/shell/scripts/shortcuts/tmux/guake.sh
@@ -3,6 +3,8 @@
 # Old one with terminator
 # exec terminator -e "/home/dvg/dotfiles/linux/shell/scripts/shortcuts/tmux/auto-tmux-resurect.sh"
 
+# Start guake and automatically reload tmux
+
 guake --rename-current-tab='workflow' --execute-command='pgrep -vx tmux > /dev/null && \
 tmux new -d -s delete-me && \
 tmux run-shell $TMUX_DIR/plugins/tmux-resurrect/scripts/restore.sh && \
