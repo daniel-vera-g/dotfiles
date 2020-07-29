5207a45bc173deafb36b82c73c38fca710baefaf
danielVg <danielveragi@gmail.com>
2020-07-29 23:31:21 +0200

    explanatory comment

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,7 @@
  #!/bin/sh
 
 # MASTER INSTALLATION FILE
-
 export DEBIAN_FRONTEND=noninteractive
+
+# Run install script and save logs in install file
 cd ~/dotfiles/linux/ && chmod +x ./install.sh && ./install.sh 2>&1 | tee install-logs.txt
