f0b18bb5c2e3c58b2024b46be90acb11211220c7
danielVg <danielveragi@gmail.com>
2021-08-29 13:07:26 +0200

    Test TBD

--- tests/apps/TEST-APPS.sh
+++
@@ -1,4 +0,0 @@
- #!/bin/sh
-
-export DEBIAN_FRONTEND=noninteractive
-cd ~/dotfiles/tests/apps && chmod +x ./helper/app-test.sh && ./helper/app-test.sh 2>&1 | tee apps-install-logs.txt
--- tests/shell/CONFIG-TESTS.sh
+++
@@ -1,4 +0,0 @@
- #!/bin/sh
-
-export DEBIAN_FRONTEND=noninteractive
-cd ~/dotfiles/tests/shell && chmod +x ./helper/config-tests.sh && ./helper/config-tests.sh 2>&1 | tee config-install-logs.txt
